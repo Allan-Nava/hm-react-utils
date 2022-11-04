e8a24f1016aae96d1c65bc825485c6fc68d6e332
lucia <lucia.esposito@ies-italia.it>
2022-11-04 16:51:01 +0100

    update index a gitignore

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 // v1
 export { TranslationsProvider, useTranslationContext } from "./context";
-export { useInterval } from "./hooks";
+export * from "./hooks";
 export { Confluent, Consumer, Subject } from "./kafka";
 export { Page, Widget, WidgetProps } from "./models";
 export * from "./utils";
